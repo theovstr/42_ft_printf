3c8d9e65482fb58b72b63a52308fe450d8ec5015
Theo Vester <theveste@paul-f4Br5s8.clusters.42paris.fr>
2023-12-03 15:28:06 +0100

    more test / -0 -> +0 . % +d, 45 -> +45 / working clean

--- src/main.c
+++ src/main.c
@@ -7,4 +7,7 @@ int main(void)
 	ft_printf("+4555 : '%+d'\n", 4555);
 	ft_printf("-2147483647 : '%+d'\n", -2147483647);
 	ft_printf("' 12' : '%     i'\n", 12);
+	ft_printf("+45 : '%+  #d'\n", 45);
+	ft_printf("' 3647' : '%       d'\n", 3647);
+	ft_printf("'+0' : '%   ++++++  i'\n", -0);
 }
